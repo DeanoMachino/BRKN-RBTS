49eb7e8454c2280595deac5ca4d465c1661ec7c4
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2015-10-02 21:01:38 +0100

    Initial project commit. Added SFML to the project.

---
+++ BrokenStreetFighter/BrokenStreetFighter/main.cpp
@@ -0,0 +1,30 @@
+// BrokenStreetFighter
+// ADGS Game Jam - October 2015
+// Dean, Matt & Sydney
+// ===============================
+// main.cpp
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "Broken Street Fighter");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
